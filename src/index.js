8011e67c687972ea96e463c9a1fcb1c695eb098c
Giulia Lake <glake@ip-192-168-245-77.ec2.internal>
2018-06-28 11:08:26 -0400

    bundling styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, loaded 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.classList.add('hello');
+        
   return element;
 }
 
